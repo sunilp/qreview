6e240f412de8f0e85c6fe95c9b44f5ec0e540d01
sunilp <prakashsunil@gmail.com>
2013-06-01 01:20:04 +0530

    ss

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,8 +5,15 @@
 create table user_dtl (
   username                        	varchar(255) not null,
   name                      		varchar(255),
-  password                      	varchar(255),
-  designation                      	varchar(255),
+  password                      varchar(255),
+  designation                   varchar(255),
+  supervisor                    varchar(255),
+  lastlogin                     timestamp,
+  groupname                     varchar(255),
+  alias                         varchar(255),
+  alt_email                     varchar(255),
+  psnl_email                    varchar(255),
+  pri_email                     varchar(255),
   constraint pk_user primary key (username))
 ;
 
@@ -16,8 +23,8 @@ create table review_dtl (
   type                      	  varchar(255),
   severity                      	varchar(255),
   fileName                      	varchar(255),
-  summery                      	varchar(255),
-  description                      	varchar(4000),
+  summery                      	varchar(2000),
+  description                      	varchar(6000),
   phase                      	varchar(255),
   assignedTo                      	varchar(255),
   author                      	varchar(255),
@@ -52,6 +59,59 @@ create sequence review_dtl_history_seq start with 1000;
 create index ix_user_review_1 on review_dtl (reviewerId);
 
 
+
+
+
+create table task_dtl (
+  id                            varchar(255) primary key,
+  owner                         varchar(255),
+  todo                          varchar(255),
+  status                        varchar(255),
+  details                       varchar(255),
+  due                           timestamp,
+  createdAt                     timestamp,
+  closedAt                      timestamp,
+  done                          varchar(1),   
+  author                        varchar(255),
+  tskgroup                         varchar(10),
+  active                        varchar(1)
+  )
+;
+
+create table task_group_dtl (
+  id                            varchar(10) primary key,
+  name                          varchar(255),
+  details                       varchar(1000)
+  )
+;
+
+create table role_dtl (
+id                varchar(10) primary key,
+name              varchar(255)
+)
+;
+
+
+create table user_role_xref (
+username                varchar(255),
+role_id                 varchar(10),
+active                  varchar(1)
+)
+
+;
+
+
+create sequence task_dtl_seq start with 1000;
+
+create sequence role_dtl_seq start with 1000;
+
+create sequence task_group_dtl_seq start with 1000;
+
+
+
+create index ix_task_owner_1 on task_dtl (owner);
+
+
 # --- !Downs
 
 SET REFERENTIAL_INTEGRITY FALSE;
@@ -69,4 +129,29 @@ drop sequence if exists user_dtl_seq;
 drop sequence if exists review_dtl_seq;
 
 drop sequence if exists review_dtl_history_seq;
+
+
+
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists user_dtl;
+
+drop table if exists task_dtl;
+
+drop table if exists task_group_dtl;
+
+drop table if exists role_dtl;
+
+drop table if exists user_role_xref;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists user_dtl_seq;
+
+drop sequence if exists task_dtl_seq;
+
+drop sequence if exists role_dtl_seq;
+
+drop sequence if exists task_group_dtl_seq;
 
